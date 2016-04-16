1cda7a826023a58a8f40710d11e489a3d99128a0
tchewik <elenachistov@gmail.com>
2016-04-16 21:34:34 +0300

    .

--- client/main.js
+++ client/main.js
@@ -2,3 +2,11 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+
+
+//не работает, и черт с ней
+Meteor.startup(function() {
+  Tracker.autorun(function() {
+    console.log('There are ' + Posts.find().count() + ' posts');
+  });
+});
