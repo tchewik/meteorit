041af357127cdf3b11f215c294bcdb0d6bd5abfe
tchewik <elenachistov@gmail.com>
2016-04-19 01:33:19 +0300

    анимация удаления

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -8,18 +8,9 @@ Template.postItem.helpers({
 
 Template.postItem.events({
 	'click #js-del-btn':function(event){
-		alert("post_item вещает: " + this._id);
-		//$(event.target).css("width", "500px"); //меняет свойства кнопки
 		var postId = this._id;
-		Meteor.call('postRemove', postId);
+		$("#"+postId).hide('slow', function(){
+			Meteor.call('postRemove', postId);
+		})		
 	}
 });
-
-
-
-/*
-$("#js-del-btn").click(function(){
-	alert("post_item says: js-del-btn is clicked");
-	Posts.remove(_id);
-});
-*/
