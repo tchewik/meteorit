1267f7e4e752c8e2a99478b5a5716ad30492e666
tchewik <elenachistov@gmail.com>
2016-04-20 23:09:14 +0300

    добавлен параметр рейтинга, вывод на страницу

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -12,5 +12,11 @@ Template.postItem.events({
 		$("#"+postId).hide('slow', function(){
 			Meteor.call('postRemove', postId);
 		})		
+	},
+	'click .js-rate':function(event){
+		var rating = $(event.currentTarget).data("userrating");
+		console.log("post_item.js says: " + rating);
+		var postId = this.id;
+		Meteor.call('postRating', postId, rating);
 	}
 });
