3506958771558d06b157958609b577e6a2d50b39
tchewik <elenachistov@gmail.com>
2016-04-23 23:55:18 +0300

    изменен механизм выбора локали

--- client/helpers/language.js
+++ client/helpers/language.js
@@ -1,15 +1,8 @@
 Meteor.startup(function() {
-	// обращается к местоположению устройства и в зависимости от него выставляет язык
-	$.getJSON('//ipinfo.io', function(userData){
-	  console.log(userData);
-	  country = userData.country;
-	  if (country == 'RU'
-	  	  || country == 'UA'
-	  	  || country == 'BY'
-	  	  || country == 'KZ'){
-	  	return i18n.setLanguage('ru');
-	  }
-	});
-	// по умолчанию английский
-	i18n.setLanguage('en');
+	var localeFromBrowser = window.navigator.userLanguage || window.navigator.language;
+  	var locale = 'en';
+  	if (localeFromBrowser.match(/ru/)) {
+    	locale = 'ru';
+    }
+  	i18n.setLanguage(locale);
 });
