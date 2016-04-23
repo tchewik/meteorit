9c1d3cadf8b1712869f5953f64547fd1390e74a0
tchewik <elenachistov@gmail.com>
2016-04-23 18:58:23 +0300

    +clientside

--- server/publications.js
+++
@@ -1,9 +0,0 @@
-/*
-Meteor.publish('posts', function(author){
-	return Posts.find({author: author}); // Корень зла!
-});
-*/
-
-Meteor.publish('posts', function(){
-	return Posts.find({}, {sort:{rating:-1}});
-});
