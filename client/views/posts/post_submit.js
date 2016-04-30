6ed7e06bf85409ac2616992fcbda55082e76e16e
tchewik <elenachistov@gmail.com>
2016-04-30 20:29:51 +0300

    Исправлено удаление постов. Удаляет только создатель

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -2,21 +2,20 @@ Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
 
-    var post = {
+    var postAttributes = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
-    Router.go('postsList');
-    Meteor.call('postInsert', post, function(error, result) {
+    //Router.go('postsList');
+    Meteor.call('postInsert', postAttributes, function(error, result) {
       // отобразить ошибку пользователю и прерваться
       if (error)
       	return alert(error.reason);
 
       if (result.postExists)
       	alert('This link has already been posted');
-      
-      //Router.go('postPage', {_id: result._id});  
-      
+    
+      Router.go('postPage', {_id: result._id});  
     });
   }
 });
