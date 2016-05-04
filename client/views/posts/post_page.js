0195166e03f95f980e118f616a2825b95d131eb0
tchewik <elenachistov@gmail.com>
2016-05-04 05:55:22 +0300

    Добавлена страница с комментариями пользователей (NR)

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -1,5 +1,5 @@
-Template.postPage.events({
-	'click h2': function(){
-		console.log("HEY");
+Template.postPage.helpers({
+	comments: function() {
+		return Comments.find({postId: this._id});
 	}
 })
