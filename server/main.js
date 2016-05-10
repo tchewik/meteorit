914d13c8a84482f8af2217f32ae47874c5b7d02b
tchewik <elenachistov@gmail.com>
2016-05-10 21:39:35 +0300

    Исправлено различие полей при разных входах, доработан вид страницы

--- server/main.js
+++ server/main.js
@@ -3,3 +3,10 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
 
 });
+
+Accounts.onCreateUser(function(options, user) {
+	user.profile = {
+		name: options.username
+	}
+	return user;
+})
