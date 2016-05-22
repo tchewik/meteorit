c8ec9f8099fd473526b7b377f27a419de77a9c6d
tchewik <elenachistov@gmail.com>
2016-05-22 21:31:33 +0300

    Добавлено: вернуться со страницы отдельного поста

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -2,4 +2,10 @@ Template.postPage.helpers({
 	comments: function() {
 		return Comments.find({postId: this._id}, {sort:	{'submitted': 1}});
 	},
-})
+});
+
+Template.postPage.events({
+	'click .go-back':function(event){
+		history.back();
+	}
+});
