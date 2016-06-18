b03096e156c2d85a5dbfd6cad65f2c2c336f00f3
tchewik <elenachistov@gmail.com>
2016-06-18 15:15:32 +0300

    simple regexp

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -2,4 +2,7 @@ Template.postPage.helpers({
 	comments: function() {
 		return Comments.find({postId: this._id}, {sort:	{'submitted': 1}});
 	},
+	text: function() {
+		return text.replace(/((https?|ftps?|file|smb):\/\/[a-z.:/]+)/gi, '<a href="$1">$1</a>');
+	}
 });
